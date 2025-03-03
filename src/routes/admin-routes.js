0a4e407dd79d7d2dedf8bfaa4a21aaae6fe499b5
blessed0314 <christian.daniel999@gmail.com>
2025-03-03 04:45:27 -0500

    Add swagger open-api

--- src/routes/admin-routes.js
+++ src/routes/admin-routes.js
@@ -6,13 +6,112 @@ const updateUserController = require('../controllers/common/updateUserController
 
 const adminRouter = Router();
 
-
+/**
+ * @swagger
+ * /admin/users:
+ *   get:
+ *     summary: Retrieve a list of users
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A list of users
+ */
 adminRouter.get('/users', getUsersController);
 
+/**
+ * @swagger
+ * /admin/user/{id}:
+ *   get:
+ *     summary: Retrieve a single user by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The user ID
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A single user
+ */
 adminRouter.get('/user/:id', getUserController);
 
+/**
+ * @swagger
+ * /admin/user/{id}:
+ *   put:
+ *     summary: Update a user by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The user ID
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               roles:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       description: Role ID
+ *     responses:
+ *       200:
+ *         description: User updated
+ */
 adminRouter.put('/user/:id', updateUserController);
 
+/**
+ * @swagger
+ * /admin/create:
+ *   post:
+ *     summary: Create a new user
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               roles:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       description: Role ID
+ *     responses:
+ *       201:
+ *         description: User created
+ */
 adminRouter.post('/create', createUserController);
 
 adminRouter.use((req, res, next) => {
